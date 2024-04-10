4f4bf4da2a697c92d46c3272b7e68ae2b775f6b0
Спас Цилков <spas.tzilkov@gmail.com>
2024-04-10 12:29:30 +0300

    1

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int n = int.Parse(Console.ReadLine());
+           int sum = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
