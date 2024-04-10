dc3dffe97fd67248be8ba9523df21e6300cd5b6a
Спас Цилков <spas.tzilkov@gmail.com>
2024-04-10 13:25:13 +0300

    Course

--- Week1/Program.cs
+++ Week1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine(Prime(17));
+            Console.WriteLine(Prime(17));
             Console.WriteLine(Sum());
         }
         public static bool Prime(int n)
@@ -29,6 +29,32 @@
             }
             return result;
         }
+        public static bool Course()
+        {
+            string[] euroArr = Console.ReadLine().Split(",");
+            string[] usdArr = Console.ReadLine().Split(",");
 
+            if(euroArr.Length != usdArr.Length)
+            {
+                Console.WriteLine("invalid input");
+            }
+            decimal[] euroToLv = new decimal[euroArr.Length];
+            decimal[] usdToLv = new decimal[usdArr.Length];
+
+            for (int i = 0; i < euroArr.Length; i++)
+            {
+                euroToLv[i] = Decimal.Parse(euroArr[i]) * 1.95m;
+                usdToLv[i] = Decimal.Parse(usdArr[i]) * 1.8m;
+            }
+
+            for(int i = 0;i < usdArr.Length;i++)
+            {
+                if (euroToLv[i] != usdToLv[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
